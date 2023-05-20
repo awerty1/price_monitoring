385ea342c7f236e678140ffe830fd1edae320457
pheno <testuniversity@yandex.ru>
2023-05-20 17:35:00 +0300

    1. changed smtp_server to "smtp.example.com"

---
+++ config_example.py
@@ -0,0 +1,22 @@
+# config maps
+
+'''
+to chromedriver
+'''
+# Указываем URL-адрес страницы
+url = "https://www.somesite.com/path/path/detail.aspx?size=1018191290"
+path_to_chromedriver = "C:/path/to/chromedriver/folder/"
+
+'''
+to smtp server
+'''
+# smtp server
+smtp_server = 'smtp.example.com'
+# port for smtp
+smtp_port = 587
+# login for email
+login = 'somebody@example.com'
+# psswd for email
+password = 'psswd'
+# email to send msg
+to = 'somebody@example.com'
