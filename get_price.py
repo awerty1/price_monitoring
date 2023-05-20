e562f805a1d6b9f03f98f7d105037d3c3666fe95
pheno <testuniversity@yandex.ru>
2023-05-20 18:22:58 +0300

    1. The logic of the chromedriver function has been moved to the get_price file 2. remove counter = 1 from main function 3. created main() function in main.py

---
+++ get_price.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+# my functions
+import config
+import config_example
+import send_email
+import messages
+import file_manipulations
+
+url = config.url
+path_to_chromedriver = config.path_to_chromedriver
+
+# Specify the path to the Chromedriver
+chromedriver_path = webdriver.chrome.service.Service(executable_path=path_to_chromedriver)
+
+# variable for counter
+counter = 1
+# chromedriver_path =
+
+
+def get_price_from_site():
+    global counter
+
+    # Saving a price from a file
+    saved_price = file_manipulations.read_price_from_file()
+
+    # Settings for launching the browser in "headless" mode
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+
+    # Launch the browser with the specified settings
+    driver = webdriver.Chrome(service_log_path='NUL', service=chromedriver_path, options=options)
+
+    driver.get(url)
+    # driver.refresh()
+    time.sleep(3)
+    page = driver.page_source
+    driver.quit()
+
+    soup = BeautifulSoup(page, 'html.parser')
+    container = soup.find_all('ins', attrs={
+        'class': 'price-block__final-price'})
+    # print(container[0].text.replace(' ', ''))
+
+    # Check the price change
+    current_price = container[0].text.replace("₽", '').replace(' ', '')
+    if current_price != saved_price:
+        msg = messages.changed_price_msg(counter, saved_price, current_price)
+        file_manipulations.save_price_changes_to_file(counter, msg)
+        send_email.send_email_to(saved_price, current_price)
+    else:
+        msg = messages.price_did_not_changed(counter, current_price)
+        file_manipulations.save_price_changes_to_file(counter, msg)
+
+    counter += 1
+
+    # Save the new price in the file
+    file_manipulations.save_price_to_file(current_price)
