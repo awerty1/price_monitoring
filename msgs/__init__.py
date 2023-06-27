4e5da498c5cf6f5f45851786e11bb4c73c225c1b
pheno <testuniversity@yandex.ru>
2023-06-28 01:46:10 +0300

    1. added marketplace 2. remove messages.py to package msgs

---
+++ msgs/__init__.py
@@ -0,0 +1,5 @@
+from .messages import get_datetime
+from .messages import start_msg
+from .messages import changed_price_msg
+from .messages import price_did_not_changed
+from .messages import price_of_selected_items_did_not_changed
